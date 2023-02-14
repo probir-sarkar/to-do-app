3ce274d3d4ba42fb45b93eaa09be187e59fc96b1
Probir Sarkar <probirsarkar855@gmail.com>
2023-02-14 14:13:07 +0530

    final

--- src/redux/to-do/toDoInitialStage.ts
+++ src/redux/to-do/toDoInitialStage.ts
@@ -1,35 +1,29 @@
 import { nanoid } from "nanoid";
 // use local storage to store the data
-export const toDoInitialStage = () => {
-  const initialState = [
-    {
-      id: nanoid(),
-      text: "Learn React",
-      completed: false,
-    },
-    {
-      id: nanoid(),
-      text: "Learn Redux",
-      completed: false,
-    },
-    {
-      id: nanoid(),
-      text: "Learn Redux-Toolkit",
-      completed: false,
-    },
-    {
-      id: nanoid(),
-      text: "Learn React-Redux",
-      completed: true,
-    },
-    {
-      id: nanoid(),
-      text: "Learn React-Redux-Toolkit",
-      completed: true,
-    },
-  ];
-
-const storedToDoList = localStorage.getItem("toDoList");
-return storedToDoList ? JSON.parse(storedToDoList) : initialState;
-
-};
+export const toDoInitialStage = [
+  {
+    id: nanoid(),
+    text: "Learn React",
+    completed: false,
+  },
+  {
+    id: nanoid(),
+    text: "Learn Redux",
+    completed: false,
+  },
+  {
+    id: nanoid(),
+    text: "Learn Redux-Toolkit",
+    completed: false,
+  },
+  {
+    id: nanoid(),
+    text: "Learn React-Redux",
+    completed: true,
+  },
+  {
+    id: nanoid(),
+    text: "Learn React-Redux-Toolkit",
+    completed: true,
+  },
+];
