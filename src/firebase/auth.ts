a5d3597c38b7cee497a8442595d0877abbac9e5d
Probir Sarkar <probirsarkar855@gmail.com>
2023-02-19 14:46:03 +0530

    WORKING

--- src/firebase/auth.ts
+++ src/firebase/auth.ts
@@ -1,4 +1,13 @@
 import { getAuth, onAuthStateChanged } from "firebase/auth";
 import { app } from "./firebase";
-
+import { store } from "../redux/store";
+import { setUser } from "../redux/user/userSlice";
 export const auth = getAuth(app);
+
+onAuthStateChanged(auth, (user) => {
+  if (user) {
+    store.dispatch(setUser({ email: user.email, uid: user.uid }));
+  } else {
+    store.dispatch(setUser({ email: "" }));
+  }
+});
