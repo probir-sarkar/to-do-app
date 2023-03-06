b941f2da057f4b46066aae25e1b968be496c1594
Probir Sarkar <probirsarkar855@gmail.com>
2023-03-06 13:47:23 +0530

    to do modify

--- src/components/to-do-list/to-do-list.tsx
+++ src/components/to-do-list/to-do-list.tsx
@@ -37,41 +37,37 @@ const ToDoList = ({}) => {
 
   return (
     <div className="h-full overflow-y-auto mb-20">
-      {/* to do title with style */}
-      <h2 className="text-xl md:text-3xl font-bold text-gray-900 border-b-2 md:border-b-4 border-gray-900 py-2 my-4 ">
-        To Do
-      </h2>
+      {uncompletedListItems.length !== 0 && (
+        <>
+          <h2 className="text-xl md:text-3xl font-bold text-gray-900 border-b-2 md:border-0 border-gray-900 py-2 my-4 ">
+            To Do
+          </h2>
+          {uncompletedListItems.map((item: toDoItemInterface) => (
+            <ul>
+              <li key={item.id}>
+                <ListItem item={item} />
+              </li>
+            </ul>
+          ))}
+        </>
+      )}
 
-      <ul>
-        {uncompletedListItems.length === 0 ? (
-          <li>
-            <p className="text-gray-700">No items to do</p>
-          </li>
-        ) : (
-          uncompletedListItems.map((item: toDoItemInterface) => (
-            <li key={item.id}>
-              <ListItem item={item} />
-            </li>
-          ))
-        )}
-      </ul>
       {/* Completed Task */}
-      <h2 className="text-xl md:text-3xl font-bold text-gray-900 border-b-2 md:border-b-4 border-gray-900 py-2 my-8">
-        Completed Task
-      </h2>
-      <ul>
-        {completedListItems.length === 0 ? (
-          <li>
-            <p className="text-gray-700">No items completed</p>
-          </li>
-        ) : (
-          completedListItems.map((item: toDoItemInterface) => (
-            <li key={item.id}>
-              <ListItem item={item} />
-            </li>
-          ))
-        )}
-      </ul>
+
+      {completedListItems.length !== 0 && (
+        <>
+          <h2 className="text-xl md:text-3xl font-bold text-gray-900 border-b-2 md:border-0 border-gray-900 py-2 my-8">
+            Completed Task
+          </h2>
+          {completedListItems.map((item: toDoItemInterface) => (
+            <ul>
+              <li key={item.id}>
+                <ListItem item={item} />
+              </li>
+            </ul>
+          ))}
+        </>
+      )}
     </div>
   );
 };
